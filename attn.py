0ddebdb12b3f571b357c32f88506180ef7a3d878
jaeioursh <joshc1010@gmail.com>
2024-02-12 12:24:29 -0800

    working attention now

--- attn.py
+++ attn.py
@@ -14,9 +14,21 @@ def scaled_dot_product_attention(query, key, value) -> torch.Tensor:
     attn_weight = torch.softmax(attn_weight, dim=-1)
     return attn_weight @ value,attn_weight
 
+def getPositionEncoding(seq_len, d, n=10000):
+    P = np.zeros((seq_len, d))
+    for k in range(seq_len):
+        for i in np.arange(int(d/2)):
+            denominator = np.power(n, 2*i/d)
+            P[k, 2*i] = np.sin(k/denominator)
+            P[k, 2*i+1] = np.cos(k/denominator)
+    return P
+
+
+
 class net(nn.Module):
-    def __init__(self,idim=1,odim=1,hidden=20):
+    def __init__(self,idim,odim,hidden=20):
         super(net, self).__init__()
+
 
         self.wK=nn.Linear(idim,hidden)
         self.wQ=nn.Linear(idim,hidden)
@@ -34,13 +46,13 @@ class net(nn.Module):
         Q=self.wQ(x)
         V=self.wV(x)
         
-        res,attn=scaled_dot_product_attention(Q,K,V)
+        res,attn=scaled_dot_product_attention(Q,Q,Q)
         
-        #res=F.scaled_dot_product_attention(Q,K,V)
+        #res,attn=F.scaled_dot_product_attention(Q,K,V),none
         #attn=res
         if not attn_idx:
-            return self.wout2(res)
-            #return self.wout2(self.acti(self.wout1(res)))
+            #return self.wout2(res)
+            return self.wout2(self.acti(self.wout1(res)))
         else:
             return attn
         
@@ -62,22 +74,30 @@ class net(nn.Module):
         return loss.detach().item()
     
 
-atn=net(1,1,2)
+atn=net(2,2,20)
 
-x=[[[1],[0],[0],[0]],
-   [[0],[1],[0],[0]],
-   [[0],[0],[1],[0]],
-   [[0],[0],[0],[1]]]
+x=[[[1,0],[0,0],[0,0],[0,0]],
+   [[0,0],[1,0],[0,0],[0,0]],
+   [[0,0],[0,0],[1,0],[0,0]],
+   [[0,0],[0,0],[0,0],[1,0]]]
 
-y=[[[0],[1],[0],[0]],
-   [[0],[0],[1],[0]],
-   [[0],[0],[0],[1]],
-   [[1],[0],[0],[0]]]
-x,y=[x[0]],[y[0]]
-x,y=np.array(x),np.array(y)
+y=[[[0,0],[1,0],[0,0],[0,0]],
+   [[0,0],[0,0],[1,0],[0,0]],
+   [[0,0],[0,0],[0,0],[1,0]],
+   [[1,0],[0,0],[0,0],[0,0]]]
+
+P=getPositionEncoding(4,2)
+print(P)
+
+#x,y=[x[0]],[y[0]]
+x,y=np.array(x,dtype=float),np.array(y,dtype=float)
+print(x.shape,P.shape)
+x[:]+=P
+print(x)
 
 for i in range(3001):
-    print(atn.train(x,y))
+    l=atn.train(x,y)
+    print(l)
 
     if i%1000==0:
         print(np.round(atn.feed(x),3))
